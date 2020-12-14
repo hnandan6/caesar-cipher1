dc93bd40c709ab72c2673966d80bb5cb7732c1b1
hnandan6 <hnandan6@gatech.edu>
2020-12-14 04:04:40 +0000

    phase 3

--- main.py
+++ main.py
@@ -5,23 +5,18 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text,shift_amount):
+def caesar(plain_text, shift_amount, option):
   chipher_text = ""
   for letter in plain_text:
     position = alphabet.index(letter)
-    new_position = position +shift_amount
+    if option == "encode":
+      new_position = position +shift_amount
+    elif option == "decode":
+      new_position = position -shift_amount
     new_letter = alphabet[new_position]
     chipher_text += new_letter
-  print(f"The encoded text is {chipher_text}")
-
-def decode(plain_text,shift_amount):
-  chipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    new_letter = alphabet[new_position]
-    chipher_text += new_letter
-  print(f"The decoded text is {chipher_text}")
+  
+  print(f"The {option}d text is {chipher_text}")  
 
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
@@ -37,7 +32,4 @@ def decode(plain_text,shift_amount):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-if direction == "encode":
-  encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-  decode(plain_text = text, shift_amount = shift)
+caesar(text,shift,direction)
